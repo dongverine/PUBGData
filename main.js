28e4ace9c5e6c2b4429df442888a8393c457537f
taeho <taeho0523@gmail.com>
2022-07-21 18:00:53 +0900

    dd

--- main.js
+++ main.js
@@ -1,18 +1,96 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
-    }
-    if(request.url == '/favicon.ico'){
+var url = require('url');
+
+
+var app = http.createServer(function (request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    
+   
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            
+            fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
+                var title = 'For The Squard';
+                var description = 'Hello, Node.js';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">PUBG Statistic</a></h1>
+                <ul>
+                  <li><a href="/?id=all">전체</a></li>
+                </ul>
+                <h2><a>순위</a></h2>
+                <ol>
+                  <img src="/img" width="40%">
+                  <li><a href="/?id=person1">사람1</a></li>
+                  <li><a href="/?id=person2">사람2</a></li>
+                  <li><a href="/?id=person3">사람3</a></li>
+                  <li><a href="/?id=person4">사람4</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>
+                  <img src="/img" width="40%"/>
+                </p>
+                </body>
+                </html>
+                
+                `;
+                response.writeHead(200);    
+                response.end(template);
+            } );
+        } else {
+            fs.readFile(`data/${queryData.id}`,'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">PUBG Statistic</a></h1>
+                <ul>
+                  <li><a href="/?id=all">전체</a></li>
+                </ul>
+                <h2><a>순위</a></h2>
+                <ol>
+                  <img src="/img" width="40%"/>
+                  <li><a href="/?id=person1">사람1</a></li>
+                  <li><a href="/?id=person2">사람2</a></li>
+                  <li><a href="/?id=person3">사람3</a></li>
+                  <li><a href="/?id=person4">사람4</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>
+                </p>
+                </body>
+                </html>
+                
+                `;
+                response.writeHead(200);
+                response.end(template);
+            } );
+        }
+        
+    }else{
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
- 
+
+    
+    
+    
+
 });
 app.listen(3000);
