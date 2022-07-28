7348870b38f4cc3dbcd30ffeeed11389cff19f42
dongverine <dongverine@gmail.com>
2022-07-29 00:30:30 +0900

    test에서도 랭킹을 볼수있게 변경

--- test/test.root.js
+++ test/test.root.js
@@ -6,10 +6,60 @@ describe('TEST ROOT', () => {
     //this.timeout(15000);
     it('URL Call Test', async () => {
         //console.log("test start");
+
         let testApi = new NickelbackPubgAPI();
-        let responseJson = await testApi.getPlayerRankList("dators,bleumer102,77cloud,gasip");
-        //let responseJson = testApi.getPlayerRankList("77cloud");
-        console.log("test return");
+        let sortedRankUserList = [];
+        let responseJson = {};
+        try { 
+            responseJson = await testApi.getPlayerRankList("dators,bleumer102,77cloud,gasip");
+        }catch(error){
+            console(error);
+        }
+        //배열에 넣는다.
+        for(let accountId in responseJson){
+            let userInfo = responseJson[accountId];
+            sortedRankUserList.push(userInfo);
+        }
+        //점수 순서대로 배열 sort
+        sortedRankUserList.sort(function(user1, user2){
+            console.log("sorting :",user1.name+"["+user1.currentRankPoint+"]", user2.name+"["+user2.currentRankPoint+"]");
+            if(user1.errorMessage!=null){
+                return 1;
+            }
+            if(user2.errorMessage!=null){
+                return -1;
+            }                    
+            if(user1.currentRankPoint == user2.currentRankPoint){
+                return 0;
+            }else
+            if(user1.currentRankPoint > user2.currentRankPoint){
+                return -1;
+            }else{
+                return 1;
+            }
+        });
+        //console.log("sortedRankUserList : ",sortedRankUserList);
+        /*
+            {
+                name: 'dators',
+                currentTier: { tier: 'Gold', subTier: '3' },
+                currentRankPoint: 2204,
+                errorMessage : ''
+            },                    
+        */
+        var templateUserInfo = "";
+
+        sortedRankUserList.forEach((userInfo, idx)=>{
+            if(userInfo.errorMessage==null){
+                templateUserInfo += `\n[ ${userInfo.currentRankPoint} ] ${userInfo.name} ${userInfo.currentTier.tier} ${userInfo.currentTier.subTier}`;
+            }else{
+                templateUserInfo += `\n > Error ${userInfo.errorMessage} ${userInfo.name} }`;                        
+            }
+        });
+
+        console.log("\n\n\n\n=======[TeamKill is my life] Rank======")
+        console.log(templateUserInfo);
+
     });
     it('sample', () => {
       assert.equal([1, 2, 3].indexOf(2), 1);
